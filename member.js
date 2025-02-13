c03bfcfad47490010ea506d7012606a51b73689b
Pasi Aarnilähde <83806781+pasaarni@users.noreply.github.com>
2025-02-13 14:28:44 +0000

    add about section

--- member.js
+++ member.js
@@ -1,3 +1,7 @@
+// About: This file contains the implementation of the member functionalities
+// for the application. It includes methods for creating, updating, and 
+// managing member data.
+
 function skillsMember() {
   return {
     restrict: 'E',
